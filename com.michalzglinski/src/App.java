f7ebb37e30e414ff89b6cdf7ab43ba1948e35597
Michal3008 <michalzz203@gmail.com>
2021-02-16 17:38:38 +0100

    first commit

---
+++ com.michalzglinski/src/App.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        /* PODAWANIE SWOJEGO IMIENIA 
+
+
+        PODAWANIE SWOJEGO IMIENIA 
+        System.out.println("Podaj swoję imię");
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.nextLine();
+   
+        System.out.println("Twoje imię to " + firstName );
+        
+        */
+
+        /*  KALKULATOR DWOÓCH LICZB
+
+        System.out.println("Podaj pierwszą liczbę");
+        int a = scanner.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        int b = scanner.nextInt();
+        System.out.println(a+"b = a + b");
+    
+
+
+        scanner.close();
+        */
+
+        int nr = 2;
+        if(nr==2){
+            System.out.println("Liczba to 2 ");
+        }else{
+            System.out.println("Liczba to 3 ");
+        }else{
+            System.out.println("Jakaś inna liczba niż 3 i 2 ");
+
+
+
+
+
+            
+
+
+
+    scanner.close();
+}
