64296a6e673a3545f7bbf78b1d90f20bed143082
Michal3008 <michalzz203@gmail.com>
2021-02-19 18:31:22 +0100

    gra

--- Frame.java
+++ Frame.java
@@ -1,16 +1,19 @@
 import javax.swing.JFrame;
 
-public class Frame extends JFrame{
-  private static final long serialVersionUID = 1L;
-  public Frame(){
-    super("Kółko i krzyżyk");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(300,400);
-    setLocation(700,100);
-    setVisible(true);
-  }
+public class Frame extends JFrame
+{
+    private static final long serialVersionUID = 1L;
+    public Frame()
+    {
+        super("kolko i krzyzyk");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLocation(300, 100);
+
+
+        setVisible(true);
+    }
 }
- 
 
     
 
